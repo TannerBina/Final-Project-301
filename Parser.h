cbbd4df6a397425430354ba1ea61e8fcb612ea28
m-sisco <marissa.sisco@richmond.edu>
2017-04-05 12:51:15 -0400

    Parser Updated

--- Parser.h
+++ Parser.h
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <map>
 #include "Instruction.h"
+#include <algorithm>
 
 
 using namespace std;
@@ -14,10 +15,22 @@ public:
 	Parser(string file);
 	void parseConfig(string file);
 	void parseProgram();
-	void parseMemory();
-	void parseRegister();
+	map<string, string> parseMemory();
+	map<int, string> parseRegister();
+
+	string getConfig(string key);
+	string getMem(string key);
+	string getReg(int key);
+
+	map<int, int> getRegMap();
+
+	void convertMemory();
+	void convertRegister();
+
 
 private:
 	map <string, string> config;
+	map <string, int> mem;
+	map <int, int> regMem;
 
 };
