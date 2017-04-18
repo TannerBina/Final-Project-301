ccbcda75d033ab67d62ae326956f89e7d57db745
afrikanjoe <JM@Josephs-MacBook-Air.local>
2017-04-18 16:08:06 -0400

    More edits

--- Pipeline/ALU.cpp
+++ Pipeline/ALU.cpp
@@ -1,9 +1,35 @@
 #include "ALU.h"
+
 using namespace std;
 
-ALU::ALU(string control, int reg1, int reg2){
+ALU::ALU(string control, int src1, int src2){
 
-	performALU(control, reg1, reg2);
+	performALU(control, src1, src2);
 }
 
+int ALU:performALU(string control, int  src1, int src2){
 
+	int output;
+	if(control == "010"){
+		output = src1 + src2; // return the sum
+
+	}
+	else if(control == "110"){
+		output = src1 - src2; // return the subtraction
+	}
+	else if(control == "000"){
+		output = src1 & src2; //return the bitwise AND
+	}
+	else if(control == "001"){
+		output = src1 | src2;// return the bitwise OR
+	}
+	else if(control == "111"){// return SLT, 
+		if (src1 < src2){
+			output = 1;
+		}else{
+			output = 0;
+		}
+		// might change depending on if Src2 is signed unsigned?
+	}
+
+}
