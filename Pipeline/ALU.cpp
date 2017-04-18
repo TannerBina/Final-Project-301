344054630226ad65b03b4a647018bcd4ad617a6b
afrikanjoe <JM@Josephs-MacBook-Air.local>
2017-04-18 19:24:30 -0400

    long yo

--- Pipeline/ALU.cpp
+++ Pipeline/ALU.cpp
@@ -4,32 +4,45 @@ using namespace std;
 
 ALU::ALU(string control, int src1, int src2){
 
-	performALU(control, src1, src2);
+ 	performALU(control, src1, src2)
+
+
 }
 
+//store out into output
+//store
 int ALU:performALU(string control, int  src1, int src2){
-
-	int output;
+	int out;
 	if(control == "010"){
-		output = src1 + src2; // return the sum
+		out = src1 + src2; // return the sum
 
 	}
 	else if(control == "110"){
-		output = src1 - src2; // return the subtraction
+		out= src1 - src2; // return the subtraction
 	}
 	else if(control == "000"){
-		output = src1 & src2; //return the bitwise AND
+		out = src1 & src2; //return the bitwise AND
 	}
 	else if(control == "001"){
-		output = src1 | src2;// return the bitwise OR
+		out = src1 | src2;// return the bitwise OR
 	}
 	else if(control == "111"){// return SLT, 
 		if (src1 < src2){
-			output = 1;
+			out = 1;
 		}else{
-			output = 0;
+			out = 0;
 		}
-		// might change depending on if Src2 is signed unsigned?
+	}
+	
+
+
+}
+int ALU::line(LineInfo in, int out) {
+	if (in.output == 0) {
+		in.zeroOrOne = 1;
+	}else{
+		in.zeroOrOne = 0;
 	}
 
 }
+
--- Pipeline/ALU.h
+++ Pipeline/ALU.h
@@ -10,25 +10,28 @@ ALU - takes Three inputs outputs the result
 -data input 1 int from either signextend or register table
 -data input 2 int frome either signextend or register table
 output the result of the operation(AND, OR, ADD, Subtract, SLT) performed on the inputs
+and zero or one depending on 
 */
 
 using namespace std;
 
-class ALU{
+struct LineInfo{
+	int output;
+	int zeroOrOne;
+};
 
-private:
-	int result;
+class ALU{
 
 public:
 
-	ALU();
-
-	ALU(string control, int reg1, int reg2);
+	ALU(){};
+	ALU(string control, int src1, int src2);
 
 	//given a control string taken from ALUcontrol, and registers,
 	//performs the correct operation based on the ALU control
 	//and returns int with the selected action on the input
-	int performALU(string control, int reg1, int reg2);
+	int performALU(string control, int src1, int src2);
+	int ALU::line(LineInfo in);
 	
 };
 
