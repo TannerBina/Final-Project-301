e7bd9decff85a48468db98eec819af552c79d8a7
m-sisco <marissa.sisco@richmond.edu>
2017-04-18 19:26:00 -0400

    sorry tanner

--- Pipeline/Processor.cpp
+++ Pipeline/Processor.cpp
@@ -1,11 +1,12 @@
 #include "Parser.h"
 #include "Processor.h"
 
-Processor::Processor()
+/*Processor::Processor()
 :single_step(false), debug_mode(false), print_memory_contents(false),
 write_to_file(false){
 
 }
+*/
 
 int main(int argc, char const* argv[]) {
 
@@ -15,12 +16,17 @@ int main(int argc, char const* argv[]) {
 	parser.convertRegister();
 	parser.parseProgram();
 
+	map<int, int> regist = parser.getRegMap();
+	map<int, int> memory = parser.getMemMap();
+	map<int, string> program = parser.getProgMap();
+
 	cout << parser.getConfig("program_input") << endl;
 	cout << parser.getConfig("memory_contents_input") << endl;
-	cout << parser.getMem("10000030") << endl;
-	cout << parser.getReg("14")<< endl;
-	cout << parser.getMem("1000004c") << endl;
-	cout << parser.getProg(4194308) << endl;
+	cout << parser.getConfig("output_mode") << endl;
+	cout << memory[268435520] << endl;
+	cout << regist[14]<< endl;
+	cout << memory[268435532] << endl;
+	cout << program[4194308] << endl;
 
 	
 	return 0;
